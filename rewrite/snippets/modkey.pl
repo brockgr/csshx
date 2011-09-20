13121847e9856eca0abaeb6087cd3cb795ca9064
Gavin Brock <gavin@brock-family.org>
2011-09-20 17:10:13 +0900

    Distilled the essence of this script.

--- rewrite/snippets/modkey.pl
+++ rewrite/snippets/modkey.pl
@@ -4,14 +4,12 @@ use strict;
 use warnings;
 use Foundation;
 
-@SBApplication::ISA =
-@NSEvent::ISA =
- qw(PerlObjCBridge);
-NSBundle->bundleWithPath_('/System/Library/Frameworks/ScriptingBridge.framework')->load;
-my $terminal = SBApplication->applicationWithBundleIdentifier_("com.apple.terminal");
+@NSEvent::ISA = qw(PerlObjCBridge);
+
+NSBundle->bundleWithPath_('/System/Library/Frameworks/AppKit.framework')->load;
 
 while (1) {
-warn NSEvent->modifierFlags
+  warn NSEvent->modifierFlags
 }
 
 
