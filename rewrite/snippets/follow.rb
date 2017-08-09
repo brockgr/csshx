d53eb7a4da936ea512f7c574fae50839f9d8f97c
Carlo Marcelo Arenas Belón <carenas@gmail.com>
2017-08-08 20:13:35 -0700

    Rewrite Ruby code for move_slaves_to_master_space
    Migrate code from the obsoleted 'dl' to Fiddle and avoid calls that are
    returning errors

--- rewrite/snippets/follow.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/ruby
-require 'dl';
-dl = DL::dlopen('/System/Library/Frameworks/ApplicationServices.framework/ApplicationServices')
-con = dl.sym("_CGSDefaultConnection", 'I').call()
-ARGV.map! {|wid| Integer(wid)}
-r,rs = dl.sym("CGSGetWindowWorkspace", 'IIIi').call(con[0], ARGV.shift, 0)
-dl.sym("CGSMoveWorkspaceWindowList", 'IIAII').call(con[0], ARGV, ARGV.length, rs[2])
